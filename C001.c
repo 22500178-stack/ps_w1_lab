5166677ccfb2a1ffb67575d536d663324bca1eb5
22500178 <22500178@handong.ac.kr>
2026-03-09 23:02:50 +0900

    first commit

---
+++ C001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    int stars, blank1, blank2;
+    int i, j;
+
+    // 1. 크기 입력
+    scanf("%d", &size);
+
+    // 2. 상단 V자 부분
+    for (i = 0; i < size; i++) {
+        blank1 = i;
+        blank2 = (size - 1 - i) * 2;
+        
+        for (j = 0; j < blank1; j++) printf(" ");
+        printf("*");
+        for (j = 0; j < blank2; j++) printf(" ");
+        printf("*\n");
+    }
