0a622363aeea849e4327074af338c5e33d895493
22500178 <22500178@handong.ac.kr>
2026-03-09 23:08:01 +0900

    second commit

--- C001.c
+++ C001.c
@@ -19,5 +19,13 @@ int main() {
         printf("*\n");
     }
 
+    // 3. 중단 직사각형 부분 (size + 1 줄)
+    for (i = 0; i < size + 1; i++) {
+        for (j = 0; j < size * 2; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
 
 }
